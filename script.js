ebcb602e05a6b9b4a9d593bddd2ce0a7e5bf3200
brixmabz <brixmabz14@gmail.com>
2022-09-27 15:43:34 +0800

    customized webgazer.js to apply chrome storage

--- script.js
+++ script.js
@@ -3,5 +3,23 @@ window.saveDataAcrossSessions = true;
 webgazer
   .setGazeListener((data, timestamp) => {
     if (data === null) return;
+
+    let currentElement = document.elementFromPoint(data.x, data.y);
+
+    if (currentElement === null) return;
+
+    // console.log(currentElement);
   })
   .begin();
+
+// chrome.storage.local.set({ key: value }, function () {
+//   console.log("Value is set to " + value);
+// });
+
+// chrome.storage.local.get(["test_var", "webgazer_data"], function (result) {
+//   console.log(JSON.parse(result.test_var));
+// });
+
+window.onbeforeunload = function () {
+  webgazer.saveCurrentCalibrationData();
+};
