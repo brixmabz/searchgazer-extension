745ef4e53ee940f7f774925fe01185ae7fcbb339
brixmabz <brixmabz14@gmail.com>
2023-01-14 15:48:49 +0800

    added points average feature

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.saveDataAcrossSessions = true;
+let pastSecond = 0;
+let testArr = [];
+
+createCircle();
 
 webgazer
   .setGazeListener((data, timestamp) => {
@@ -9,6 +13,26 @@ webgazer
     if (currentElement === null) return;
 
     // console.log(currentElement);
+
+    if (pastSecond !== parseInt(timestamp / 500)) {
+      pastSecond = parseInt(timestamp / 500);
+      let xTotal = 0;
+      let yTotal = 0;
+      testArr.forEach((loopData, index) => {
+        xTotal += loopData.x;
+        yTotal += loopData.y;
+      });
+      xTotal /= testArr.length;
+      yTotal /= testArr.length;
+      testArr = [];
+
+      moveCircle(xTotal, yTotal);
+    } else {
+      testArr.push({
+        x: data.x,
+        y: data.y,
+      });
+    }
   })
   .begin();
 
@@ -23,3 +47,35 @@ webgazer
 window.onbeforeunload = function () {
   webgazer.saveCurrentCalibrationData();
 };
+
+function createCircle() {
+  let circle = document.createElement("div");
+  circle.id = "gazer-circle";
+  circle.style.width = "30px";
+  circle.style.height = "30px";
+  circle.style.backgroundColor = "blue";
+  circle.style.borderRadius = "50%";
+  circle.style.position = "absolute";
+  circle.style.top = `-10%`;
+  circle.style.left = `-10%`;
+  circle.style.transform = "translate(-50%, -50%)";
+  circle.style.transition = "all .5s ease";
+  document.body.appendChild(circle);
+}
+
+function moveCircle(x, y) {
+  let xPos = parseInt((x / window.innerWidth) * 100);
+  let yPos = parseInt((y / window.innerHeight) * 100);
+  let circle = document.getElementById("gazer-circle");
+  if (circle) {
+    circle.style.top = `${yPos}%`;
+    circle.style.left = `${xPos}%`;
+  }
+}
+
+function removeCircle() {
+  let circle = document.getElementById("gazer-circle");
+  if (circle) {
+    document.body.removeChild(circle);
+  }
+}
