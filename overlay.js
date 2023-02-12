abbb10ade53735626fb57e0026255dd29f46e9c5
brixmabz <brixmabz14@gmail.com>
2023-02-12 22:49:11 +0800

    added overlay and scroller function

---
+++ overlay.js
@@ -0,0 +1,57 @@
+function overlay() {
+  var overlay = document.createElement("div");
+  overlay.className = "overlay";
+  overlay.innerHTML = `
+    <button id="clicker" class="click-icon">
+      <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          d="M128 40c0-22.1 17.9-40 40-40s40 17.9 40 40V188.2c8.5-7.6 19.7-12.2 32-12.2c25.3 0 46 19.5 47.9 44.3c8.5-7.7 19.8-12.3 32.1-12.3c25.3 0 46 19.5 47.9 44.3c8.5-7.7 19.8-12.3 32.1-12.3c26.5 0 48 21.5 48 48v32 64c0 70.7-57.3 128-128 128l-16 0H240l-.1 0h-5.2c-5 0-9.9-.3-14.7-1c-55.3-5.6-106.2-34-140-79L8 336c-13.3-17.7-9.7-42.7 8-56s42.7-9.7 56 8l56 74.7V40zM240 304c0-8.8-7.2-16-16-16s-16 7.2-16 16v96c0 8.8 7.2 16 16 16s16-7.2 16-16V304zm48-16c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16s16-7.2 16-16V304c0-8.8-7.2-16-16-16zm80 16c0-8.8-7.2-16-16-16s-16 7.2-16 16v96c0 8.8 7.2 16 16 16s16-7.2 16-16V304z"
+        />
+      </svg>
+    </button>
+    <button id="scroller" class="click-icon">
+      <svg
+        fill="#ffffff"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"
+      >
+        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"
+        />
+      </svg>
+    </button>
+  `;
+
+  $("body").append(overlay);
+}
+
+function scrollerFunc() {
+  let scrollerCont = document.createElement("div");
+  scrollerCont.className = "scroller-container";
+  scrollerCont.innerHTML = `
+    <svg
+      fill="#ffffff"
+      class="scroller-icon scroller-up"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <path
+        d="M201.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 173.3 54.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
+      />
+    </svg>
+    <svg
+      fill="#ffffff"
+      class="scroller-icon scroller-down"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <path
+        d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+      />
+    </svg>
+  `;
+
+  $("body").append(scrollerCont);
+}
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.saveDataAcrossSessions = true;
 let pastSecond = 0;
-let testArr = [];
+let pointsData = [];
+let gazeCounter = 0;
+let scrollerEnabled = false;
 
 createCircle();
 
@@ -8,32 +10,55 @@ webgazer
   .setGazeListener((data, timestamp) => {
     if (data === null) return;
 
-    let currentElement = document.elementFromPoint(data.x, data.y);
-
-    if (currentElement === null) return;
-
     // console.log(currentElement);
 
     if (pastSecond !== parseInt(timestamp / 500)) {
       pastSecond = parseInt(timestamp / 500);
       let xTotal = 0;
       let yTotal = 0;
-      testArr.forEach((loopData, index) => {
+      pointsData.forEach((loopData, index) => {
         xTotal += loopData.x;
         yTotal += loopData.y;
       });
-      xTotal /= testArr.length;
-      yTotal /= testArr.length;
-      testArr = [];
+      xTotal /= pointsData.length;
+      yTotal /= pointsData.length;
+      pointsData = [];
 
       moveCircle(xTotal, yTotal);
+
+      if (xTotal >= window.innerWidth * 0.85) {
+        $(".overlay").css("right", "0");
+      } else {
+        $(".overlay").css("right", "-15%");
+      }
+
+      if (xTotal && yTotal) {
+        let currentElement = document.elementFromPoint(xTotal, yTotal);
+
+        if (currentElement) {
+          scrollerFunction(currentElement);
+        }
+
+        if (scrollerEnabled) {
+          if (yTotal >= window.innerHeight * 0.85) {
+            window.scrollBy(0, 50);
+          } else if (yTotal <= window.innerHeight * 0.15) {
+            window.scrollBy(0, -50);
+          }
+        }
+      }
+
+      // if (currentElement === null) return;
+
+      // console.log(currentElement.closest("a"));
     } else {
-      testArr.push({
+      pointsData.push({
         x: data.x,
         y: data.y,
       });
     }
   })
+  .showPredictionPoints(false)
   .begin();
 
 // chrome.storage.local.set({ key: value }, function () {
@@ -71,8 +96,38 @@ function removeCircle() {
   }
 }
 
+function scrollerFunction(currentElement) {
+  if (currentElement.id == "scroller") {
+    if (!scrollerEnabled) {
+      $("button#scroller").css("background-color", "green");
+    } else {
+      $("button#scroller").css("background-color", "red");
+    }
+    gazeCounter += 1;
+    if (gazeCounter === 3) {
+      scrollerEnabled = !scrollerEnabled;
+      if (scrollerEnabled) {
+        $("button#scroller").css("background-color", "green");
+        scrollerFunc(scrollerEnabled);
+      } else {
+        $("button#scroller").css("background-color", "transparent");
+        $(".scroller-container").remove();
+      }
+    }
+  } else {
+    gazeCounter = 0;
+    if (!scrollerEnabled) {
+      $("button#scroller").css("background-color", "transparent");
+    } else {
+      $("button#scroller").css("background-color", "green");
+    }
+  }
+}
+
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.from === "popup" && msg.subject == "calibrate") {
     showCalibration();
   }
 });
+
+overlay();
