1c6c98500641ff15139ac395fe770cef23d1cc7e
brixmabz <brixmabz14@gmail.com>
2023-05-09 09:39:44 +0800

    changed calibration click from 10 to 20

--- calibrate.js
+++ calibrate.js
@@ -1,6 +1,6 @@
 function showCalibration() {
   alert("Please click all the red circles until they turn green.");
-  let click_required = 10;
+  let click_required = 20;
   let outer_div = document.createElement("div");
   outer_div.className = "outer-container";
 
@@ -58,7 +58,7 @@ function isCalibrationDone() {
   let done = true;
 
   $(".circle-btn").each(function (i, obj) {
-    if ($(this).data("value") < 10) {
+    if ($(this).data("value") < 20) {
       done = false;
     }
   });
