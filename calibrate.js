ee5c6cf050a27be90c54431ae6b2937219db209d
brixmabz <brixmabz17@yahoo.com>
2023-06-26 01:08:40 +0800

    added auto calibration

--- calibrate.js
+++ calibrate.js
@@ -1,6 +1,36 @@
+let calibSecondsTimer = 5
+let circleElementsCustomIDs = []
+let timer = 10;
+
+function calibrationInstructions() {
+  let ins = document.createElement("div");
+  let message = `
+    <div>
+      Please look or gaze on circles which are colored green <span></span> for ${calibSecondsTimer} seconds starting from TOP-LEFT and will go from left to right, line by line.
+    </div>
+    <div class='timer'>
+      Calibration will start in ${timer} seconds
+    </div>`
+  ins.className = "calib-instructions"
+  ins.innerHTML = message;
+
+  document.body.appendChild(ins)
+
+  let intervalContainer = setInterval(() => {
+    if (!timer) {
+      clearInterval(intervalContainer);
+      $(".calib-instructions").remove()
+      timer = 10;
+    }
+
+    timer -= 1;
+
+    $("div.timer").html(`Calibration will start in ${timer} seconds`)
+  }, 1000)
+}
+
 function showCalibration() {
-  alert("Please click all the red circles until they turn green.");
-  let click_required = 20;
+  calibrationInstructions()
   let outer_div = document.createElement("div");
   outer_div.className = "outer-container";
   clickEnabled = false;
@@ -16,62 +46,74 @@ function showCalibration() {
     ["5%", "95%"],
     ["50%", "95%"],
     ["95%", "95%"],
-
-    // ["96px", "54px"],
-    // ["960px", "54px"],
-    // ["1824px", "54px"],
-    // ["96px", "540px"],
-    // ["960px", "540px"],
-    // ["1824px", "540px"],
-    // ["96px", "900px"],
-    // ["960px", "900px"],
-    // ["1824px", "900px"],
   ];
 
   coordinates.forEach((data, index) => {
     let html = document.createElement("div");
     html.className = "circle-btn";
     html.id = "calib-btns";
-    html.dataset.value = "0";
+    html.innerHTML = "5s";
+    html.dataset.seconds = `${calibSecondsTimer}`;
+    html.dataset.customId = `${data[0]} ${data[1]}`;
     html.style.left = `${data[0]}`;
     html.style.top = `${data[1]}`;
+
+    circleElementsCustomIDs.push(`${data[0]} ${data[1]}`)
 
     outer_div.appendChild(html.cloneNode(true));
 
     if (index === coordinates.length - 1) {
       document.body.appendChild(outer_div);
-      $(document).on("click", "#calib-btns", function () {
-        // Do something with `$(this)`.
-        let clicks = $(this).data("value");
-        $(this).data("value", clicks + 1);
-        if (clicks === click_required) {
-          $(this).css("background-color", "#48e148");
-        }
-
-        isCalibrationDone();
-      });
+      setTimeout(() => {
+        autoCalibration()
+        recordData()
+      }, (timer + 1) * 1000)
     }
   });
 }
 
-function updateButton(el) {}
-
-function isCalibrationDone() {
-  let done = true;
-
-  $(".circle-btn").each(function (i, obj) {
-    if ($(this).data("value") < 20) {
-      done = false;
-    }
-  });
-
-  if (done) {
+function autoCalibration() {
+  if (!circleElementsCustomIDs.length) {
     webgazer.pause();
     webgazer.saveCurrentCalibrationData();
     setTimeout(function(){
       $(".outer-container").remove();
       createCircle();
       delayClick();
-    }, 3000); 
+    }, 2000);
+    return
+  };
+
+  let el = $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`);
+  el.css("background-color", "#48e148");
+
+  let elSeconds = parseInt(el.attr("data-seconds"));
+
+  if (elSeconds > 0) {
+    el.attr("data-seconds", elSeconds - 1);
+    el.html(`${elSeconds - 1}s`);
+  } else {
+    circleElementsCustomIDs.shift();
+    $(el).remove()
+    $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`).css("background-color", "#48e148");
   }
+
+  setTimeout(() => {
+    requestAnimationFrame(autoCalibration)
+  }, 1000)
+}
+
+function recordData() {
+  let recordDataInterval = setInterval(() => {
+    if (!circleElementsCustomIDs.length) {
+      clearInterval(recordDataInterval)
+      return
+    }
+
+    let elem = $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`)
+    const xPos = $(elem).offset().left + ($(elem).width() / 2);
+    const yPos = $(elem).offset().top + ($(elem).height() / 2);
+
+    webgazer.recordScreenPosition(xPos, yPos, "click");
+  }, 300)
 }
