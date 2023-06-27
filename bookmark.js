befa0408bb80dc68ff969ab27fa69ec90c7e0832
brixmabz <brixmabz14@gmail.com>
2023-06-27 12:19:33 +0800

    fixed bookmark not clickable

--- bookmark.js
+++ bookmark.js
@@ -6,16 +6,16 @@ function bookmarkFunc() {
   <div class="bookmark-container">
     <span class="bookmark-title">Bookmarks</span>
     <div class="bookmark-list">
-      <div class="bookmark-item" data-url="https://eye-tracking-youtube.com.trigl-demo.com/">
+      <div class="bookmark-item webgazer-clickable" data-url="https://eye-tracking-youtube.com.trigl-demo.com/">
         <h1>YouTube</h1>https://eye-tracking-youtube.com.trigl-demo.com/
       </div>
-      <div class="bookmark-item" data-url="https://tictactoe.com.trigl-demo.com/">
+      <div class="bookmark-item webgazer-clickable" data-url="https://tictactoe.com.trigl-demo.com/">
         <h1>Tic Tac Toe</h1>https://tictactoe.com.trigl-demo.com/
       </div>
-      <div class="bookmark-item" data-url="https://eye-tracking-look-to-speak-web.com.trigl-demo.com/">
+      <div class="bookmark-item webgazer-clickable" data-url="https://eye-tracking-look-to-speak-web.com.trigl-demo.com/">
         <h1>Look To Speak</h1>https://eye-tracking-look-to-speak-web.com.trigl-demo.com/
       </div>
-      <div class="bookmark-item" data-url="https://eye-tracking-food-menu.com.trigl-demo.com/">
+      <div class="bookmark-item webgazer-clickable" data-url="https://eye-tracking-food-menu.com.trigl-demo.com/">
         <h1>Food Menu</h1>https://eye-tracking-food-menu.com.trigl-demo.com/
       </div>
     </div>
