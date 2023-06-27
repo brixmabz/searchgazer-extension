2ce05aca22e61498dc8e46d716013600b0a981f4
brixmabz <brixmabz14@gmail.com>
2023-06-27 12:21:23 +0800

    updated calibration

--- calibrate.js
+++ calibrate.js
@@ -1,6 +1,7 @@
-let calibSecondsTimer = 5
-let circleElementsCustomIDs = []
+let calibSecondsTimer = 5;
+let circleElementsCustomIDs = [];
 let timer = 10;
+let isDelaying = false;
 
 function calibrationInstructions() {
   let ins = document.createElement("div");
@@ -33,6 +34,12 @@ function showCalibration() {
   calibrationInstructions()
   let outer_div = document.createElement("div");
   outer_div.className = "outer-container";
+
+  let lookMessageElem = document.createElement("div");
+  lookMessageElem.className = "look-message";
+  lookMessageElem.id = "look-message-cont";
+  lookMessageElem.innerHTML = "Look Here";
+
   clickEnabled = false;
   removeCircle();
 
@@ -48,6 +55,8 @@ function showCalibration() {
     ["95%", "95%"],
   ];
 
+  outer_div.appendChild(lookMessageElem);
+
   coordinates.forEach((data, index) => {
     let html = document.createElement("div");
     html.className = "circle-btn";
@@ -57,6 +66,7 @@ function showCalibration() {
     html.dataset.customId = `${data[0]} ${data[1]}`;
     html.style.left = `${data[0]}`;
     html.style.top = `${data[1]}`;
+    html.style.opacity = `0`;
 
     circleElementsCustomIDs.push(`${data[0]} ${data[1]}`)
 
@@ -73,6 +83,7 @@ function showCalibration() {
 }
 
 function autoCalibration() {
+  isDelaying = false;
   if (!circleElementsCustomIDs.length) {
     webgazer.pause();
     webgazer.saveCurrentCalibrationData();
@@ -86,21 +97,36 @@ function autoCalibration() {
 
   let el = $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`);
   el.css("background-color", "#48e148");
+  el.css("opacity", "1");
+
+  let lookElem = $("#look-message-cont")
+  setLookMessagePosition(el)
+  lookElem.show()
 
   let elSeconds = parseInt(el.attr("data-seconds"));
 
   if (elSeconds > 0) {
     el.attr("data-seconds", elSeconds - 1);
-    el.html(`${elSeconds - 1}s`);
+    el.html(`${elSeconds}s`);
+    lookElem.html("Look Here")
+    setTimeout(() => {
+      requestAnimationFrame(autoCalibration)
+    }, 1000)
   } else {
     circleElementsCustomIDs.shift();
     $(el).remove()
-    $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`).css("background-color", "#48e148");
-  }
+    let nextEl = $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`);
+    nextEl.css("opacity", "1");
+    isDelaying = true
 
-  setTimeout(() => {
-    requestAnimationFrame(autoCalibration)
-  }, 1000)
+    setLookMessagePosition(nextEl)
+    lookElem.html("Look Here Next")
+    if (!circleElementsCustomIDs.length) lookElem.remove()
+
+    setTimeout(() => {
+      requestAnimationFrame(autoCalibration)
+    }, 3000)
+  }
 }
 
 function recordData() {
@@ -113,7 +139,30 @@ function recordData() {
     let elem = $(`div[data-custom-id="${circleElementsCustomIDs[0]}"]`)
     const xPos = $(elem).offset().left + ($(elem).width() / 2);
     const yPos = $(elem).offset().top + ($(elem).height() / 2);
-
-    webgazer.recordScreenPosition(xPos, yPos, "click");
+    
+    if (!isDelaying) {
+      webgazer.recordScreenPosition(xPos, yPos, "click");
+    }
   }, 300)
+}
+
+function setLookMessagePosition(elem) {
+  if (!circleElementsCustomIDs.length) return;
+
+  const xPos = $(elem).offset().left + ($(elem).width() / 2);
+  const yPos = $(elem).offset().top + ($(elem).height() / 2);
+
+  let lookElem = $("#look-message-cont")
+
+  if (!lookElem) return;
+
+  if(circleElementsCustomIDs.length <= 3) {
+    lookElem.removeClass("look-message")
+    lookElem.addClass("look-message-top")
+    lookElem.css("top", `${yPos - 60}px`)
+    lookElem.css("left", `${xPos}px`)
+  } else {
+    lookElem.css("top", `${yPos + 35}px`)
+    lookElem.css("left", `${xPos}px`)
+  }
 }
